5f0d747d113f25dd62789738c85234fe34e5f53b
Sergey Tkachenko <sergeyt2000@gmail.com>
2026-02-05 17:01:30 +0200

    fix file name

--- src/routers/StoresRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { storesController } from '../controllers/stores.js';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-
-const router = Router();
-
-router.get('/', ctrlWrapper(storesController));
-export default router;
